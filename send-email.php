9d13c89564f7824a4b0cb997d78ade920d020cde
loccha <charlotte.locas@hotmail.com>
2024-06-23 10:55:03 -0400

    Version securisee

--- send-email.php
+++ send-email.php
@@ -6,12 +6,12 @@ use PHPMailer\PHPMailer\SMTP;
 
 require 'vendor/autoload.php';
 
-$surname = $_POST["surname"];
-$name = $_POST["name"];
-$child_surname = $_POST["child-surname"];
-$child_name = $_POST["child-name"];
-$phone = $_POST["phone"];
-$email = $_POST["email"];
+$surname = htmlspecialchars($_POST["surname"], ENT_QUOTES, 'UTF-8');
+$name = htmlspecialchars($_POST["name"], ENT_QUOTES, 'UTF-8');
+$child_surname = htmlspecialchars($_POST["child-surname"], ENT_QUOTES, 'UTF-8');
+$child_name = htmlspecialchars($_POST["child-name"], ENT_QUOTES, 'UTF-8');
+$phone = htmlspecialchars($_POST["phone"], ENT_QUOTES, 'UTF-8');
+$email = htmlspecialchars($_POST["email"], ENT_QUOTES, 'UTF-8');
 $age = $_POST["age"];
 $scolaire = $_POST["scolaire"];
 $lundi = isset($_POST["lundi"]) ? 'Lundi: disponible' : 'Lundi: indisponible';
@@ -19,7 +19,7 @@ $mardi = isset($_POST["mardi"]) ? 'Mardi: disponible': 'Mardi: indisponible';
 $mercredi = isset($_POST["mercredi"]) ? 'Mercredi: disponibile': 'Mercredi: indisponible';
 $jeudi = isset($_POST["jeudi"]) ? 'Jeudi: disponibile': 'Jeudi: indisponible';
 $vendredi = isset($_POST["vendredi"]) ? 'Vendredi: disponibile': 'Vendredi: indisponible'; 
-$message = $_POST["message"];
+$message = htmlspecialchars($_POST["message"], ENT_QUOTES, 'UTF-8');
 
 
 
@@ -42,7 +42,7 @@ try {
 
     //dest address
     $mail->addAddress('charlotte.locas@hotmail.com', 'Charlotte');
-    //$mail->addAddress('camillelocasorthophoniste@hotmail.com', 'Camille');
+    //$mail->addAddress('camille.orthophonie@hotmail.com', 'Camille');
 
 
     $mail->isHTML(true);
