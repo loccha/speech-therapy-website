895cf8aceb7004b50e8db5d0b51f2d589cfe683d
loccha <charlotte.locas@hotmail.com>
2026-02-05 07:47:24 -0500

    Rename french names to engish
    Rename several form fields/checkboxes in rendez-vous.html (select name changed to "child-age"; availability inputs renamed from French ids/names to english: monday..friday) and update send-email.php to match those new names.

--- send-email.php
+++ send-email.php
@@ -14,13 +14,13 @@ $child_surname = htmlspecialchars($_POST["child-surname"], ENT_QUOTES, 'UTF-8');
 $child_name = htmlspecialchars($_POST["child-name"], ENT_QUOTES, 'UTF-8');
 $phone = htmlspecialchars($_POST["phone"], ENT_QUOTES, 'UTF-8');
 $email = htmlspecialchars($_POST["email"], ENT_QUOTES, 'UTF-8');
-$age = $_POST["age"];
-$scolaire = $_POST["scolaire"];
-$lundi = isset($_POST["lundi"]) ? 'Lundi: disponible' : 'Lundi: indisponible';
-$mardi = isset($_POST["mardi"]) ? 'Mardi: disponible': 'Mardi: indisponible';
-$mercredi = isset($_POST["mercredi"]) ? 'Mercredi: disponibile': 'Mercredi: indisponible';
-$jeudi = isset($_POST["jeudi"]) ? 'Jeudi: disponibile': 'Jeudi: indisponible';
-$vendredi = isset($_POST["vendredi"]) ? 'Vendredi: disponibile': 'Vendredi: indisponible'; 
+$child_age = $_POST["child-age"];
+$school_level = $_POST["school-level"];
+$monday = isset($_POST["monday"]) ? 'Lundi: disponible' : 'Lundi: indisponible';
+$tuesday = isset($_POST["tuesday"]) ? 'Mardi: disponible': 'Mardi: indisponible';
+$wednesday = isset($_POST["wednesday"]) ? 'Mercredi: disponibile': 'Mercredi: indisponible';
+$thursday = isset($_POST["thursday"]) ? 'Jeudi: disponibile': 'Jeudi: indisponible';
+$friday = isset($_POST["friday"]) ? 'Vendredi: disponibile': 'Vendredi: indisponible'; 
 $message = htmlspecialchars($_POST["message"], ENT_QUOTES, 'UTF-8');
 
 $send_username = $_ENV['EMAIL_SEND_USERNAME'];
@@ -54,12 +54,12 @@ try {
                       <b>Nom: </b>'. $name .'<br>
                       <b>Prénom de l\'enfant: </b> '. $child_surname .'<br>
                       <b>Nom de l\'enfant: </b>'. $child_name .'<br>
-                      <b>Âge de l\'enfant: </b> '. $age .' <br>
-                      <b>Niveau de scolarité: </b> '. $scolaire .'<br>
+                      <b>Âge de l\'enfant: </b> '. $child_age .' <br>
+                      <b>Niveau de scolarité: </b> '. $school_level .'<br>
                       <b>Numéro de téléphone: </b>'. $phone .'<br>
                       <b>Adresse courriel: </b>' . $email . '<br><br> 
                       <b>Disponibilités: </b><br>' 
-                        . $lundi . '<br>' . $mardi . '<br>' . $mercredi . '<br>' . $jeudi . '<br>' . $vendredi . '<br><br>
+                        . $monday . '<br>' . $tuesday . '<br>' . $wednesday . '<br>' . $thursday . '<br>' . $friday . '<br><br>
                       <b>Inquiétudes: </b> '. $message .'<br>';
 
     $mail->send();
